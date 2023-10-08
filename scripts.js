22d68eea604f55e642afcd2c9cd6273e820cfad7
Hokann <hokan4316v3@gmail.com>
2023-10-08 19:19:51 -0400

    create functionality for digits w/ operators, computes left to right only, expression history displayed

--- scripts.js
+++ scripts.js
@@ -1,13 +1,67 @@
+function displayResult(text){
+    const result = document.querySelector('.result');
+    result.innerText = text;
+    if (text === 'CLEAR') result.innerText = '';
+    return result.innerText;
+}
+
 function displayExpression(text){
     const equation = document.querySelector('.equation');
     equation.innerText += text;
+    if (text === 'CLEAR') equation.innerText = '';
     return equation.innerText;
 }
 
 function evaluate(string){
+    const operators = ['+', '-', '/', 'x']
     
+    let strArr = string.split('');
+    let strExp = '';
+
+    strArr.forEach((char) => {
+        if (operators.includes(char)){
+            strExp += ' '+char+' '
+        }else{
+            strExp += char;
+        }
+    });
+    let arrExp = strExp.split(' ');
+    
+    return recursion(arrExp);
+
 }
 
+
+function recursion(arrExp){
+
+    arrExp.forEach((element) => {
+        switch(element){
+            case 'x':
+                opIndex = arrExp.indexOf('x');
+                subResult = arrExp[opIndex-1]*arrExp[opIndex+1];
+                arrExp.splice(opIndex-1, 3, subResult);
+                return recursion(arrExp);
+            case '/':
+                opIndex = arrExp.indexOf('/');
+                subResult = arrExp[opIndex-1]/arrExp[opIndex+1];
+                arrExp.splice(opIndex-1, 3, subResult);
+                return recursion(arrExp);
+            case '+':
+                opIndex = arrExp.indexOf('+');
+                subResult = Number(arrExp[opIndex-1])+Number(arrExp[opIndex+1]);
+                arrExp.splice(opIndex-1, 3, subResult);
+                return recursion(arrExp);
+            case '-':
+                opIndex = arrExp.indexOf('-');
+                subResult = arrExp[opIndex-1]-arrExp[opIndex+1];
+                arrExp.splice(opIndex-1, 3, subResult);
+                return recursion(arrExp);
+        }
+        
+    })
+
+    if (arrExp.length === 1) return arrExp.join('');
+}
 
 const buttons = Array.from(document.querySelectorAll('button'));
 let expression = 0;
@@ -21,10 +75,16 @@ buttons.forEach((button) => {
         }
         console.log(expression);
 
+        if(button.className.includes('ac')){
+            displayExpression('CLEAR');
+            displayResult('CLEAR');
+        }
+
 
 
         if (button.className.includes('equal')){
-            evaluate(expression);
+            let result = evaluate(expression);
+            displayResult(result);
         }
 
     });
@@ -33,4 +93,13 @@ buttons.forEach((button) => {
 
 
 
+a = '123x123y';
+const operators = ['+', '-', '/', 'x']
 
+a.split('').forEach((char) => {
+    console.log(char)
+    switch(char){
+        case 'x': 
+            console.log('has x');
+    }
+})
