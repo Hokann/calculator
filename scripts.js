d3ee4e5cb66f4aeb6073009c43baafeb49a7ba6a
Hokann <hokan4316v3@gmail.com>
2023-10-08 22:06:26 -0400

    Complete calculator functionality  & AC, Mac Calculator style computing logic

--- scripts.js
+++ scripts.js
@@ -1,16 +1,97 @@
 function displayResult(text){
     const result = document.querySelector('.result');
-    result.innerText = text;
-    if (text === 'CLEAR') result.innerText = '';
+    
+    if (result.innerText === '0') result.innerHTML = '';
+    if (!isNaN(text) || text === '.'){
+        result.innerHTML += text;
+    }
+    if (expression.length >= 1 && digits.length <= 1) result.innerHTML = text;
+    if (text === 'CLEAR') result.innerText = '0'
     return result.innerText;
 }
 
-function displayExpression(text){
-    const equation = document.querySelector('.equation');
-    equation.innerText += text;
-    if (text === 'CLEAR') equation.innerText = '';
-    return equation.innerText;
+function evaluate2(arr){
+    let operator = arr[1];
+    switch(operator){
+        case '+':
+            arr.splice(0,3,Number(arr[0])+Number(arr[2])) //is its own array (and modifies original arr)
+            return arr; //returns modified array
+        case '-':
+            arr.splice(0,3,arr[0]-arr[2]);
+            return arr;
+        case 'x':
+            arr.splice(0,3,arr[0]*arr[2]);
+            return arr;
+        case '/':
+            arr.splice(0,3,arr[0]/arr[2]);
+            return arr;
+    }
 }
+
+const specialBtns = Array.from(document.querySelectorAll('.special'));
+let digits = [];
+let expression = [];
+let num = '';
+
+specialBtns.forEach((specialBtn) => {
+    specialBtn.addEventListener('click', () => {
+        console.log(specialBtn);
+
+        if(specialBtn.className.includes('ac')){
+            expression = [];
+            digits = [];
+            displayResult('CLEAR');
+        }
+
+        if (specialBtn.className.includes('equal')){
+            expression.push(num);
+            expression = evaluate2(expression);
+            console.log(expression);
+            digits = []; num = '';
+            displayResult('CLEAR');
+            displayResult(expression[0]);
+        }        
+    });
+});
+
+
+const operatorBtns = Array.from(document.querySelectorAll('.operator'));
+operatorBtns.forEach((operatorBtn) => {
+    operatorBtn.addEventListener('click', () => {
+        i = 0;
+        if (num !== '') expression.push(num);
+        expression.push(operatorBtn.innerText)
+        digits = [];
+        console.log(expression);
+        if (expression.length > 3){
+            expression = evaluate2(expression);
+            displayResult(expression[0]);
+            console.log(expression);
+        }
+    });
+});
+
+const digitBtns = Array.from(document.querySelectorAll('.num'));
+let i=0;
+digitBtns.forEach((digitBtn) => {
+    digitBtn.addEventListener('click', () => {
+        digits[i] = digitBtn.innerText;
+        displayResult(digits[i]);
+        i += 1
+        num = digits.join('');
+        console.log(digits)
+        console.log(num)
+    });
+});
+
+
+
+
+
+
+
+
+
 
 function evaluate(string){
     const operators = ['+', '-', '/', 'x']
@@ -62,44 +143,3 @@ function recursion(arrExp){
 
     if (arrExp.length === 1) return arrExp.join('');
 }
-
-const buttons = Array.from(document.querySelectorAll('button'));
-let expression = 0;
-
-buttons.forEach((button) => {
-    button.addEventListener('click', () => {
-        console.log(button);
-        
-        if (button.className.includes('normal')){
-            expression = displayExpression(button.innerText);
-        }
-        console.log(expression);
-
-        if(button.className.includes('ac')){
-            displayExpression('CLEAR');
-            displayResult('CLEAR');
-        }
-
-
-
-        if (button.className.includes('equal')){
-            let result = evaluate(expression);
-            displayResult(result);
-        }
-
-    });
-
-});
-
-
-
-a = '123x123y';
-const operators = ['+', '-', '/', 'x']
-
-a.split('').forEach((char) => {
-    console.log(char)
-    switch(char){
-        case 'x': 
-            console.log('has x');
-    }
-})
